c90ff6d19a9788cff9296d584dbe5803b90c6105
heather zurek and zsuzsanna mangu <heather@>
2019-03-27 16:17:18 -0700

    add styling, colors and box.

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,9 @@
+// var add = function(number1, number2) {
+//   return number1 + number2;
+// }
+
+
+
 $(function() {
   $("#info").submit(function(event) {
     var nameInput = $("input#name").val();
@@ -11,4 +17,13 @@ $(function() {
     event.preventDefault();
 
   });
+    // 
+    // $("#info").submit(function(event) {
+    //   event.preventDefault();
+    //   var number1 = parseInt($("#add1").val());
+    //   var number2 = parseInt($("#add2").val());
+    //   var result = add(number1, number2);
+    //   $("#output").text(result);
+    // });
+
 });
